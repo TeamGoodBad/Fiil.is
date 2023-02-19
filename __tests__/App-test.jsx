1265c5e48146589c718e9bf818fb742d0a42c389
Lari Nurkka <lari.nurkka@gmail.com>
2023-02-19 20:00:22 +0200

    Fixed tests

--- __tests__/App-test.jsx
+++ __tests__/App-test.jsx
@@ -5,9 +5,13 @@
 import 'react-native';
 import React from 'react';
 import App from '../App';
-
 // Note: test renderer must be required after react-native.
 import renderer from 'react-test-renderer';
+// include this line for mocking react-native-gesture-handler
+import 'react-native-gesture-handler/jestSetup';
+
+// Silence the warning: Animated: `useNativeDriver` is not supported because the native animated module is missing
+jest.mock('react-native/Libraries/Animated/NativeAnimatedHelper');
 
 it('renders correctly', () => {
   renderer.create(<App />);
