fa4bd78757a5ed5caa25889ceb18687e1802e467
Lari Nurkka <lari.nurkka@gmail.com>
2023-02-20 15:57:51 +0200

    App rename to Fiilis

--- android/app/src/main/java/com/goodbad/fiilis/MainApplication.java
+++ android/app/src/main/java/com/goodbad/fiilis/MainApplication.java
@@ -1,4 +1,4 @@
-package com.goodbad;
+package com.goodbad.fiilis;
 
 import android.app.Application;
 import com.facebook.react.PackageList;
--- ios/fiilisTests/fiilisTests.m
+++ ios/fiilisTests/fiilisTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface goodbadTests : XCTestCase
+@interface fiilisTests : XCTestCase
 
 @end
 
-@implementation goodbadTests
+@implementation fiilisTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL (^)(UIView *view))test
 {
