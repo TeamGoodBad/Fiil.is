dfc89928fac69168151d148d01515cbda6f8adcd
Lari Nurkka <lari.nurkka@gmail.com>
2023-02-27 20:16:19 +0200

    Changes to CD: - Run CD only on new release tags - Update android version number from release tag - Upload signed app package to release

--- src/screens/SettingsView.jsx
+++ src/screens/SettingsView.jsx
@@ -4,6 +4,7 @@ const SettingsView = ({navigation}) => {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
       <Text>Settings View</Text>
+      <Text>{require('../../package.json').version}</Text>
     </View>
   );
 }
