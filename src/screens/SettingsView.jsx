2685e60e7f5a6916ca3db66e147dee6f1e0235d0
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-02-27 14:29:41 +0200

    Style testing at SettingsView

--- src/screens/SettingsView.jsx
+++ src/screens/SettingsView.jsx
@@ -1,24 +1,59 @@
 import { useState } from 'react';
-import { View } from "react-native";
-import { Switch, useTheme, Text, Button} from 'react-native-paper';
+import { View, StyleSheet } from "react-native";
+import { Switch, useTheme, Button, Title, Text} from 'react-native-paper';
 
 const SettingsView = ({navigation}) => {
-  const [onOff, setOnOff] = useState(true);
+  const [isSwitchOn, setIsSwitchOn] = useState(true);
   const theme = useTheme();
-  const color = onOff ? theme.colors.tertiaryContainer : theme.colors.background
+  const color = isSwitchOn ? theme.colors.tertiaryContainer : theme.colors.background
+  
+  const onToggleSwitch = () => setIsSwitchOn(!isSwitchOn);
+  const styles = StyleSheet.create({
+    title: {
+      alignSelf: 'center',
+      paddingTop: 5,
+      paddingBottom: 5
+    },
+    content: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: color,
+    },
+    setting: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'space-around'
+    }
+  });
   
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', backgroundColor: color }}> 
-      <Text color={onOff ? theme.colors.onTertiaryContainer : 'white'}>Settings View</Text>
-      <Switch
-        color={theme.colors.onTertiaryContainer}
-        value={onOff}
-        onValueChange={() => setOnOff(onOff ? false : true)} />
-      <Button mode="outlined">
-        Jou!
-      </Button>
+    <View style={{flex:1}}> 
+      <Title style={styles.title}>
+        Settings View
+      </Title>
+      <View style={styles.content}>
+        <View style={styles.setting}>
+          <Text>Värinvaihto</Text>
+          <Switch
+            value={isSwitchOn}
+            onValueChange={onToggleSwitch} />
+        </View>
+        <View style={styles.setting}>
+          <Text>Värinvaihto</Text>
+          <Switch
+            value={isSwitchOn}
+            onValueChange={onToggleSwitch} />
+        </View>
+
+        <Button mode="outlined">
+          Nappi, joka ei tee mitään
+        </Button>
+      </View>
     </View>
   );
 }
+
+
 
 export default SettingsView;
