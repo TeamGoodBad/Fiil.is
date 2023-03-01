3b82999367235bcc23bc4aa04b5b011c927d47c2
Niko Kynsijarvi <niko.kynsija>
2023-03-01 14:22:30 +0200

    Started mainview

--- src/screens/MainView.jsx
+++ src/screens/MainView.jsx
@@ -1,15 +1,44 @@
-import { View } from "react-native";
-import { Text, TextInput } from 'react-native-paper';
+import {View} from 'react-native';
+import {Text, Button, TextInput} from 'react-native-paper';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const MainView = ({navigation}) => {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}> 
-      <Text>Main View</Text>
-      <TextInput
-        style={{height: 40}}
-      />
-    </View >
+    <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+      <View
+        style={{
+          height: '40%',
+          width: '100%',
+          alignItems: 'center',
+          justifyContent: 'center',
+          flexDirection: 'row',
+        }}>
+        <MaterialCommunityIcons name="star" size={76} />
+        <MaterialCommunityIcons name="star" size={76} />
+        <MaterialCommunityIcons name="star" size={76} />
+        <MaterialCommunityIcons name="star" size={76} />
+        <MaterialCommunityIcons name="star" size={76} />
+      </View>
+      <View
+        style={{
+          height: '55%',
+          width: '100%',
+          display: 'flex',
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <TextInput
+          multiline={true}
+          mode="outlined"
+          placeholder={'How has your day gone?'}
+          style={{height: 400, width: '100%'}}
+        />
+        <Button style={{margin: 10, width: 200}} mode="contained">
+          Save
+        </Button>
+      </View>
+    </View>
   );
-}
+};
 
 export default MainView;
