61a49db8b96df96ae5183b27e823833625ff6ff5
Niko Kynsijarvi <niko.kynsija>
2023-03-02 21:27:31 +0200

    event handler for adding stars

--- src/screens/MainView.jsx
+++ src/screens/MainView.jsx
@@ -1,23 +1,60 @@
-import {View} from 'react-native';
-import {Text, Button, TextInput} from 'react-native-paper';
+import {useState} from 'react';
+import {Pressable, View} from 'react-native';
+import {Text, Button, TextInput, useTheme} from 'react-native-paper';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 
 const MainView = ({navigation}) => {
+  const [stars, setStars] = useState([false, false, false, false, false]);
+  const theme = useTheme();
+
+  const handlePress = index => {
+    const newStars = stars.map((s, i) => {
+      if (i <= index) {
+        return true;
+      }
+      return false;
+    });
+    setStars(newStars);
+    return index;
+  };
+
   return (
-    <View style={{flex: 1, justifyContent: 'flex-start', alignItems: 'center'}}>
+    <View
+      style={{
+        flex: 1,
+        backgroundColor: theme.colors.background,
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+      }}>
       <View
         style={{
-          height: '40%',
+          display: 'flex',
+          height: '20%',
+          alignItems: 'center',
+          justifyContent: 'flex-end',
+        }}>
+        <Text variant="headlineLarge" color={theme.colors.onSurface}>
+          Miten päiväsi on mennyt?
+        </Text>
+      </View>
+      <View
+        style={{
+          height: '20%',
           width: '100%',
           alignItems: 'center',
           justifyContent: 'center',
           flexDirection: 'row',
         }}>
-        <MaterialCommunityIcons name="star" size={76} />
-        <MaterialCommunityIcons name="star" size={76} />
-        <MaterialCommunityIcons name="star" size={76} />
-        <MaterialCommunityIcons name="star" size={76} />
-        <MaterialCommunityIcons name="star" size={76} />
+        {stars.map((star, index) => {
+          return (
+            <Pressable onPress={e => handlePress(index)} key={index}>
+              <MaterialCommunityIcons
+                name={star ? 'star' : 'star-outline'}
+                size={76}
+              />
+            </Pressable>
+          );
+        })}
       </View>
       <View
         style={{
@@ -30,7 +67,7 @@ const MainView = ({navigation}) => {
         <TextInput
           multiline={true}
           mode="outlined"
-          placeholder={'How has your day gone?'}
+          placeholder={'Kerro lisää...'}
           style={{height: 400, width: '100%'}}
         />
         <Button style={{margin: 10, width: 200}} mode="contained">
