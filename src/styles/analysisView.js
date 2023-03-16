a436535ae6b84403338b3699e33e1038aed37c41
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-03-16 12:47:12 +0200

    Added basestyle.js and analysisView.js

---
+++ src/styles/analysisView.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+import { getStyle } from './baseStyle';
+
+const basestyle = getStyle();
+
+export const getStyles = (theme) => StyleSheet.create({
+    base: basestyle.container,
+    containerCenter: {
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    chipContainer: {
+        overflow: 'scroll',
+        flexDirection: 'column',
+        rowGap: 10,
+        padding: '5%',
+    }
+});
---
+++ src/styles/baseStyle.js
@@ -0,0 +1,8 @@
+import { StyleSheet } from 'react-native';
+
+export const getStyle = () => StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 10,
+    },
+});
