adea746568d6579690e259d17395c5e6c277bd44
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-03-16 22:53:26 +0200

    Showing correct amount of stars

--- src/components/Stars.jsx
+++ src/components/Stars.jsx
@@ -9,7 +9,7 @@ const WINDOW_HEIGHT = Dimensions.get('window').height;
 // editable - is it possible to change rating
 const Stars = ({ rating, editable, onChange }) => {
   const [stars, setStars] = useState(
-    Array(5).fill(true).fill(false, rating)
+    (rating >= 0) ? Array(5).fill(true).fill(false, rating+1) : Array(5).fill(false)
   );
   const theme = useTheme();
 
@@ -37,7 +37,7 @@ const Stars = ({ rating, editable, onChange }) => {
         return (
           <Pressable onPress={e => editable ? handlePress(index) : null} key={index}>
             <MaterialCommunityIcons
-              color={theme.colors.stars[("star"+index)]}
+              color={theme.colors.stars[`star${index}`]}
               name={star ? 'star' : 'star-outline'}
               size={60}
             />
