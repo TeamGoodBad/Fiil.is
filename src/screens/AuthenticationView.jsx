837aedaf0aa299f3732feed5c3078c36bbf45437
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-03-20 09:07:44 +0200

    Import style to auth view

--- src/screens/AuthenticationView.jsx
+++ src/screens/AuthenticationView.jsx
@@ -3,52 +3,11 @@ import { useTheme } from "react-native-paper";
 import CodePin from 'react-native-pin-code';
 
 import { checkPin } from "../storage/settings";
-
-const WINDOW_WIDTH = Dimensions.get('window').width;
-const WINDOW_HEIGHT = Dimensions.get('window').height;
+import { getPinStyles } from "../styles/settingsView";
 
 const AuthenticationView = ({ navigation }) => {
   const theme = useTheme();
-  const pinStyle = StyleSheet.create({
-    container: {
-      height: 150,
-      width: WINDOW_WIDTH,
-      backgroundColor: theme.colors.background,
-    },
-    containerPin: {
-      height: 40,
-      width: WINDOW_WIDTH,
-      flexDirection: 'row',
-      justifyContent: 'space-around',
-      alignItems: 'center',
-      paddingLeft: 15,
-      paddingRight: 15,
-      marginTop: 20,
-    },
-    pin: {
-      backgroundColor: theme.colors.surfaceVariant,
-      textAlign: 'center',
-      flex: 1,
-      marginLeft: 20,
-      marginRight: 20,
-      borderRadius: 5,
-      shadowColor: '#00000000',
-      shadowOffset: {width: 1,height : 1},
-      shadowRadius: 5,
-      shadowOpacity : 0.4
-    },
-    text: {
-      textAlign: 'center',
-      color: theme.colors.onBackground,
-      fontSize: 20,
-      marginTop: 30
-    },
-    error: {
-      textAlign: 'center',
-      color: theme.colors.error,
-      paddingTop: 10 
-    }
-  });
+  const pinStyle = getPinStyles(theme);
 
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
