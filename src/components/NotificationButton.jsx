abee7d6f8ad7c1022a9a357fa85bb359f93d0110
Anniina Aarnio <111737424+anniina-aarnio@users.noreply.github.com>
2023-04-17 14:10:35 +0300

    Notifications working at least one time after pushing button

--- src/components/NotificationButton.jsx
+++ src/components/NotificationButton.jsx
@@ -2,10 +2,20 @@ import React from 'react';
 import NotificationModule from '../modules/NotificationModule';
 import {Button} from 'react-native-paper';
 
+/** Creates a button with onPress-function
+ *  It sets an alarm / notification that triggers at given time
+ *  More info: ../modules/NotificationModule
+*/
 const NewModuleButton = () => {
     const onPress = () => {
         console.log('We did invoke the native module here!');
-        NotificationModule.createNotification('testiOtsikko', 'testiTeksti', 14, 2, 0);
+        NotificationModule.createNotification(
+            'testiOtsikko',
+            'testiTeksti',
+            14, //hours
+            5,  // minutes
+            0   // seconds
+        );
     };
 
     return (
--- src/modules/NotificationModule.ts
+++ src/modules/NotificationModule.ts
@@ -8,6 +8,12 @@
  * 3. Integer hours: the hour for notification (0-23)
  * 4. Integer minutes: the minute for notification (0-59)
  * 5. Integer seconds: the second for notification (0-59)
+ * 
+ * Notification will trigger at given time and the application doesn't need to
+ * be even in the background. It should be notificating daily on the same time.
+ * TODO: check if it works daily!
+ * The notification won't trigger anymore if the application is
+ * force stopped from App info, or the phone has been shut down.
  */
 
 import {NativeModules} from 'react-native';
